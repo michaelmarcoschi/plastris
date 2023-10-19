c787d302ae48cebf6088b3525c7a5b182d3836c9
michaelmarcoschi <140986983+michaelmarcoschi@users.noreply.github.com>
2023-10-19 21:00:12 +1300

    Add files via upload

--- checkLocalStorage.js
+++ checkLocalStorage.js
@@ -1,88 +1,83 @@
 const defaultSettings = {
-			
-	sensitivity :{
-		"DAS": 30,
-		"SD_DAS": 30,
-		"ARR": 40,
-		"SDR": 40
-	},
+  sensitivity: {
+    DAS: 30,
+    SD_DAS: 30,
+    ARR: 40,
+    SDR: 40,
+  },
 
-	keybinds :[
-		{event: "ROTATE_CW", trigger: "ArrowUp"},
-		{event: "ROTATE_CCW", trigger: "KeyZ"},
-		{event: "MOVE_RIGHT", trigger: "ArrowRight"},
-		{event: "MOVE_LEFT", trigger: "ArrowLeft"},
-		{event: "MOVE_DOWN", trigger: "ArrowDown"},
-		{event: "HARD_DROP", trigger: "Space"},
-		{event: "HOLD", trigger: "KeyC"},
-		{event: "PAUSE", trigger: "Escape"}
-	],
+  keybinds: [
+    { event: "ROTATE_CW", trigger: "ArrowUp" },
+    { event: "ROTATE_CCW", trigger: "KeyZ" },
+    { event: "MOVE_RIGHT", trigger: "ArrowRight" },
+    { event: "MOVE_LEFT", trigger: "ArrowLeft" },
+    { event: "MOVE_DOWN", trigger: "ArrowDown" },
+    { event: "HARD_DROP", trigger: "Space" },
+    { event: "HOLD", trigger: "KeyC" },
+    { event: "PAUSE", trigger: "Escape" },
+  ],
 
-	cosmetics: {
-		skin: 1,
-		background: 1,
-	}
-}
+  cosmetics: {
+    skin: 1,
+    background: 1,
+  },
+};
 
 const defaultRecords = [
-			
-	[
-		{label: "OVR_TOTAL_ROTATIONS", value: 0},
-		{label: "OVR_TOTAL_LOCKS", value: 0},
-		{label: "OVR_TOTAL_SCORE", value: 0},
-		{label: "OVR_TOTAL_GAMES", value: 0}
-	],
+  [
+    { label: "OVR_TOTAL_ROTATIONS", value: 0 },
+    { label: "OVR_TOTAL_LOCKS", value: 0 },
+    { label: "OVR_TOTAL_SCORE", value: 0 },
+    { label: "OVR_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "SPR_BEST_40L", value: 1000000},
-		{label: "SPR_BEST_20L", value: 1000000},
-		{label: "SPR_BEST_100L", value: 1000000},
-		{label: "SPR_TOTAL_GAMES", value: 0},
-	],
+  [
+    { label: "SPR_BEST_40L", value: 1000000 },
+    { label: "SPR_BEST_20L", value: 1000000 },
+    { label: "SPR_BEST_100L", value: 1000000 },
+    { label: "SPR_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "PL_SPR_BEST_40L", value: 1000000},
-		{label: "PL_SPR_BEST_20L", value: 1000000},
-		{label: "PL_SPR_BEST_100L", value: 1000000},
-		{label: "PL_SPR_TOTAL_GAMES", value: 0},
-	],
+  [
+    { label: "PL_SPR_BEST_40L", value: 1000000 },
+    { label: "PL_SPR_BEST_20L", value: 1000000 },
+    { label: "PL_SPR_BEST_100L", value: 1000000 },
+    { label: "PL_SPR_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "ULT_BEST_3MIN", value: 0},
-		{label: "ULT_BEST_2MIN", value: 0},
-		{label: "ULT_BEST_1MIN", value: 0},
-		{label: "ULT_TOTAL_GAMES", value: 0},
-	],
+  [
+    { label: "ULT_BEST_3MIN", value: 0 },
+    { label: "ULT_BEST_2MIN", value: 0 },
+    { label: "ULT_BEST_1MIN", value: 0 },
+    { label: "ULT_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "PL_ULT_BEST_3MIN", value: 0},
-		{label: "PL_ULT_BEST_2MIN", value: 0},
-		{label: "PL_ULT_BEST_1MIN", value: 0},
-		{label: "PL_ULT_TOTAL_GAMES", value: 0},
-	],
+  [
+    { label: "PL_ULT_BEST_3MIN", value: 0 },
+    { label: "PL_ULT_BEST_2MIN", value: 0 },
+    { label: "PL_ULT_BEST_1MIN", value: 0 },
+    { label: "PL_ULT_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "MAR_BEST_LINES_CLASSIC", value: 0},
-		{label: "MAR_BEST_SCORE_CLASSIC", value: 0},
-		{label: "MAR_BEST_LINES_FREEPLAY", value: 0},
-		{label: "MAR_BEST_SCORE_FREEPLAY", value: 0},
-		{label: "MAR_TOTAL_GAMES", value: 0},
-	],
+  [
+    { label: "MAR_BEST_LINES_CLASSIC", value: 0 },
+    { label: "MAR_BEST_SCORE_CLASSIC", value: 0 },
+    { label: "MAR_BEST_LINES_FREEPLAY", value: 0 },
+    { label: "MAR_BEST_SCORE_FREEPLAY", value: 0 },
+    { label: "MAR_TOTAL_GAMES", value: 0 },
+  ],
 
-	[
-		{label: "PL_MAR_BEST_LINES_CLASSIC", value: 0},
-		{label: "PL_MAR_BEST_SCORE_CLASSIC", value: 0},
-		{label: "PL_MAR_BEST_LINES_FREEPLAY", value: 0},
-		{label: "PL_MAR_BEST_SCORE_FREEPLAY", value: 0},
-		{label: "PL_MAR_TOTAL_GAMES", value: 0},
-	]
-
-]
+  [
+    { label: "PL_MAR_BEST_LINES_CLASSIC", value: 0 },
+    { label: "PL_MAR_BEST_SCORE_CLASSIC", value: 0 },
+    { label: "PL_MAR_BEST_LINES_FREEPLAY", value: 0 },
+    { label: "PL_MAR_BEST_SCORE_FREEPLAY", value: 0 },
+    { label: "PL_MAR_TOTAL_GAMES", value: 0 },
+  ],
+];
 
 // Set up local storage if the user doesn't already have it set
-if (localStorage.getItem("Records") == null){
-
-	localStorage.setItem("Settings", JSON.stringify(defaultSettings))
-	localStorage.setItem("Records", JSON.stringify(defaultRecords))
-	
+if (localStorage.getItem("Records") == null) {
+  localStorage.setItem("Settings", JSON.stringify(defaultSettings));
+  localStorage.setItem("Records", JSON.stringify(defaultRecords));
 }
--- play/pl-main.js
+++ play/pl-main.js
@@ -1,44 +1,44 @@
 // Override background settings with Plastic Attack theme
-BOARD_IMAGE.src = "../../../../assets/plastic-background/board.png"
-GHOST_IMAGE.src = "../../../../assets/plastic-background/ghost.png"
-HOLD_DISABLED_IMAGE.src = "../../../../assets/plastic-background/hold-disabled.png"
-INDICATOR_IMAGE.src = "../../../../assets/plastic-background/indicator.png"
+BOARD_IMAGE.src = "../../../../assets/plastic-background/board.png";
+GHOST_IMAGE.src = "../../../../assets/plastic-background/ghost.png";
+HOLD_DISABLED_IMAGE.src = "../../../../assets/plastic-background/hold-disabled.png";
+INDICATOR_IMAGE.src = "../../../../assets/plastic-background/indicator.png";
 
-canvas.setAttribute("style", "background-color: rgba(0, 0, 0, 0.5);")
-background.setAttribute("style", "background-size: cover; background-image: url(../../../../assets/plastic-background/background.png);")
+canvas.setAttribute("style", "background-color: rgba(0, 0, 0, 0.5);");
+background.setAttribute(
+  "style",
+  "background-size: cover; background-image: url(../../../../assets/plastic-background/background.png);",
+);
 
-plasticMode = true
+plasticMode = true;
 
-let currentPlasticLines = 0
-let targetPlasticLines = 0
+let currentPlasticLines = 0;
+let targetPlasticLines = 0;
 
 // Function that creates plastic lines
-function createPlasticLine(amount){
+function createPlasticLine(amount) {
+  // Create our plastic line to insert
+  let line = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8];
+  line[Math.floor(Math.random() * 10)] = 0;
 
-	// Create our plastic line to insert
-	let line = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
-	line[Math.floor(Math.random() * 10)] = 0
+  for (let i = 0; i < amount; i++) {
+    collisionMap.splice(0, 1);
+    collisionMap.splice(19, 0, line.slice());
+    currentPlasticLines++;
+  }
 
-	for (let i = 0; i < amount; i ++){
-		collisionMap.splice(0, 1)
-		collisionMap.splice(19, 0, line.slice())
-		currentPlasticLines ++
-	}
-
-	checkTopOut()
+  checkTopOut();
 }
 
 // Function that keeps the current amount of plastic lines on the board in check
-function updatePlasticLines(){
+function updatePlasticLines() {
+  currentPlasticLines = 0;
 
-	currentPlasticLines = 0
-
-	// Update the amount of plastic lines on the board
-	// Loop through every line on the collision map
-	for (y = 0; y < 20; y ++){
-
-		if (collisionMap[y].includes(8)){
-			currentPlasticLines ++
-		}
-	}
+  // Update the amount of plastic lines on the board
+  // Loop through every line on the collision map
+  for (y = 0; y < 20; y++) {
+    if (collisionMap[y].includes(8)) {
+      currentPlasticLines++;
+    }
+  }
 }
